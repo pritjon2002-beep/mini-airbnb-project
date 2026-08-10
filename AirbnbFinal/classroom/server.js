c844b470f6d01e6208ed72bf5d09c2f66580edee
pritam-demo <pritjon2002@gmail.com>
2026-08-10 07:34:57 +0545

    basic users route created

--- AirbnbFinal/classroom/server.js
+++ AirbnbFinal/classroom/server.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("expressRouter");
+});
+
+//index route
+app.get("/users", async (req, res) => {
+  console.log("get for  users");
+});
+
+//show route
+app.get("/users/:id", async (req, res) => {
+  console.log("get for show users");
+});
+
+//post route
+app.post("/users", async (req, res) => {
+  console.log("post for add users");
+});
+
+//delete route
+app.delete("/users", async (req, res) => {
+  console.log("post for delete users");
 });
 
 app.listen(8080, () => {
