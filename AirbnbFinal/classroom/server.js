55100befce71f83c964e5462fa20b2e3ff5107cd
pritam-demo <pritjon2002@gmail.com>
2026-08-11 08:22:05 +0545

    signed cookies

--- AirbnbFinal/classroom/server.js
+++ AirbnbFinal/classroom/server.js
@@ -3,8 +3,9 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+app.use(cookieParser("secretcode"));
 
+//cookies route
 app.get("/getcookies", async (req, res) => {
   res.cookie("greet", "Namaste");
   res.cookie("madeIn", "Nepal");
@@ -17,6 +18,19 @@ app.get("/greet", (req, res) => {
   res.send(`Hi,${name}`);
 });
 
+//signed cookies
+app.get("/getsignedcookies", (req, res) => {
+  res.cookie("countryName", "India", { signed: true });
+  res.send("signed cookies");
+});
+
+//verify signed cookies
+app.get("/verify", (req, res) => {
+  console.log(req.signedCookies); // print signed cookies only
+  res.send("verified");
+});
+
+//home route
 app.get("/", (req, res) => {
   res.send("expressRouter");
 });
