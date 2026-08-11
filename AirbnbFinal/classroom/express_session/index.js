c851a77ea5220c9084eff1c5591046b9087ad03e
pritam-demo <pritjon2002@gmail.com>
2026-08-11 14:55:11 +0545

    express-session simple code test

--- AirbnbFinal/classroom/express_session/index.js
+++ AirbnbFinal/classroom/express_session/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
+
+app.use(session({ secret: "express-session-secret" }));
 
 app.get("/", (req, res) => {
   res.send("welcome to express-session");
+});
+
+app.get("/test", (req, res) => {
+  res.send("test successful");
 });
 
 app.listen(8080, () => {
