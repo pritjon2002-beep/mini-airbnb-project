cc1617d0dd46eb7f51e782a346b8a2eb70e141af
pritam-demo <pritjon2002@gmail.com>
2026-08-13 16:29:42 +0545

    signup user post

--- AirbnbFinal/routes/user.js
+++ AirbnbFinal/routes/user.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
 
+//singup route
+//get route
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
+});
+
+//post route
+router.post("/signup", async (req, res) => {
+  let { username, email, password } = req.body;
+  let newUser = new User({
+    username,
+    email,
+  });
+  let registeredUser = await User.register(newUser, password);
+  console.log(registeredUser);
+  req.flash("success", "SignUp Successfully , Welcome to Listings");
+  res.redirect("/listings");
 });
 
 module.exports = router;
