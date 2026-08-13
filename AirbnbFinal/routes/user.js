b0861badb4127e684aee3d16fda4ba1d74b2296e
pritam-demo <pritjon2002@gmail.com>
2026-08-13 16:30:27 +0545

    error handling - signup user

--- AirbnbFinal/routes/user.js
+++ AirbnbFinal/routes/user.js
@@ -10,16 +10,24 @@ router.get("/signup", (req, res) => {
 });
 
 //post route
-router.post("/signup", async (req, res) => {
-  let { username, email, password } = req.body;
-  let newUser = new User({
-    username,
-    email,
-  });
-  let registeredUser = await User.register(newUser, password);
-  console.log(registeredUser);
-  req.flash("success", "SignUp Successfully , Welcome to Listings");
-  res.redirect("/listings");
-});
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      let newUser = new User({
+        username,
+        email,
+      });
+      let registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "SignUp Successfully , Welcome to Listings");
+      res.redirect("/listings");
+    } catch (error) {
+      req.flash("failure", "Username Already Used");
+      res.redirect("/signup");
+    }
+  }),
+);
 
 module.exports = router;
