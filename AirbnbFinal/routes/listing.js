6b815188ac126a6dad0a55c3945ad922b57bc35f
pritam-demo <pritjon2002@gmail.com>
2026-08-15 14:54:07 +0545

    when new listing is created owner is set to that user who create the listing

--- AirbnbFinal/routes/listing.js
+++ AirbnbFinal/routes/listing.js
@@ -39,6 +39,7 @@ router.post(
   isLoggedIn,
   wrapAsync(async (req, res, next) => {
     let newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created Successfully");
     res.redirect("/listings");
