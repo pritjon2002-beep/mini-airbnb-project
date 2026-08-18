480249b905a67a9f741de3784437b866e9ccb770
pritam-demo <pritjon2002@gmail.com>
2026-08-18 07:47:14 +0545

    router.route - user route

--- AirbnbFinal/routes/user.js
+++ AirbnbFinal/routes/user.js
@@ -5,27 +5,29 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { isLoggedIn, saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controller/users.js");
-//singup route
-//get route
-router.get("/signup", userController.renderSignupForm);
 
-//post route
-router.post("/signup", wrapAsync(userController.signup));
+//singup route
+router
+  .route("/signup")
+  //get route
+  .get(userController.renderSignupForm)
+  //post route
+  .post(wrapAsync(userController.signup));
 
 //login route
-//get route
-router.get("/login", userController.renderLoginForm);
-
-//post route
-router.post(
-  "/login",
-  saveRedirectUrl,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  wrapAsync(userController.login),
-);
+router
+  //get route
+  .route("/login")
+  .get(userController.renderLoginForm)
+  //post route
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    wrapAsync(userController.login),
+  );
 
 //user logout
 
