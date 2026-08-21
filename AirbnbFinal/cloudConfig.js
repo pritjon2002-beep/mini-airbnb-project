946b337b36706d720b18baef69c28f8b6ca616cf
pritam-demo <pritjon2002@gmail.com>
2026-08-21 06:55:19 +0545

    by fixing cloudconfig we successfully store files in cloudinary

--- AirbnbFinal/cloudConfig.js
+++ AirbnbFinal/cloudConfig.js
@@ -3,15 +3,15 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  cloud_key: process.env.CLOUD_API_KEY,
-  cloud_secret: process.env.CLOUD_API_SECRET,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "Wanderlust",
-    allowedFormat: ["png", "jpeg", "jpg"], // supports promises as well
+    allowed_formats: ["png", "jpeg", "jpg"], // supports promises as well
   },
 });
 
