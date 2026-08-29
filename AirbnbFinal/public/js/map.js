1333ad44adf3e30dd329f05e3db76a2d41020db1
pritam-demo <pritjon2002@gmail.com>
2026-08-29 09:00:30 +0545

    simple fix

--- AirbnbFinal/public/js/map.js
+++ AirbnbFinal/public/js/map.js
@@ -1,7 +1,7 @@
 // map render script
 
 const coordinates = listingGeometry.coordinates; // [lng,lat] - GeoJSON order
-const map = L.map("map").setView([coordinates[1], coordinates[0]], 15); //leaflet want [lat , lng]
+const map = L.map("map").setView([coordinates[1], coordinates[0]], 10); //leaflet want [lat , lng]
 
 // tile that images to map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
