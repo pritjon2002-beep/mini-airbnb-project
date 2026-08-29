3c68b1a1e280f38cddfe8382d4b1d4c079440bea
pritam-demo <pritjon2002@gmail.com>
2026-08-29 06:50:29 +0545

    using axios , nominatim ,geoJSON  we successfully extract location string to coordinates and show the location in map

--- AirbnbFinal/public/js/map.js
+++ AirbnbFinal/public/js/map.js
@@ -1,9 +1,10 @@
 // map render script
 
-const map = L.map("map").setView([27.71, 85.32], 15);
+const coordinates = listingGeometry.coordinates; // [lng,lat] - GeoJSON order
+const map = L.map("map").setView([coordinates[1], coordinates[0]], 15); //leaflet want [lat , lng]
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors",
 }).addTo(map);
 
-L.marker([27.71, 85.32]).addTo(map);
+L.marker([coordinates[1], coordinates[0]]).addTo(map);
