be039b8e472f35fbdcdec6fa990edf2b38da914f
pritam-demo <pritjon2002@gmail.com>
2026-08-29 07:26:53 +0545

    comments

--- AirbnbFinal/public/js/map.js
+++ AirbnbFinal/public/js/map.js
@@ -3,10 +3,12 @@
 const coordinates = listingGeometry.coordinates; // [lng,lat] - GeoJSON order
 const map = L.map("map").setView([coordinates[1], coordinates[0]], 15); //leaflet want [lat , lng]
 
+// tile that images to map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors",
 }).addTo(map);
 
+// marker color make to red
 const redIcon = L.icon({
   iconUrl:
     "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
@@ -15,10 +17,12 @@ const redIcon = L.icon({
   popupAnchor: [1, -34],
 });
 
+// mark the place
 const marker = L.marker([coordinates[1], coordinates[0]], {
   icon: redIcon,
 }).addTo(map);
 
+//popup
 marker.bindPopup(
   `Welcome to <b>${listingTitle}</b><br>Exact Location is provided after booking!`,
 );
