188c93c2933a304c3214e6cd9de8a690a9b59d01
pritam-demo <pritjon2002@gmail.com>
2026-09-01 10:50:07 +0545

    debug env

--- AirbnbFinal/app.js
+++ AirbnbFinal/app.js
@@ -29,6 +29,7 @@ const { error } = require("console");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const dbURL = process.env.MONGO_URL;
+console.log("DB URL from env:", process.env.MONGO_URL);
 
 // 2. Database Connection
 async function database() {
