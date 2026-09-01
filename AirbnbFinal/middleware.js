3b742b5770e2190b7d1e8e5f7c629495334b8e4c
pritam-demo <pritjon2002@gmail.com>
2026-09-01 16:28:28 +0545

    review author fix

--- AirbnbFinal/middleware.js
+++ AirbnbFinal/middleware.js
@@ -56,6 +56,10 @@ module.exports.validateReview = (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
   let { id, reviewId } = req.params;
   let review = await Review.findById(reviewId);
+  if (!review) {
+    req.flash("error", "Review does not exist!");
+    return res.redirect(`/listings/${id}`);
+  }
   if (!review.author._id.equals(res.locals.currUser._id)) {
     req.flash("error", "You are not the author of this review");
     return res.redirect(`/listings/${id}`);
