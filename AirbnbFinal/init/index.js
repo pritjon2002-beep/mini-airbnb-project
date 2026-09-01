9414baffced450cb897d1259261283808c92a543
pritam-demo <pritjon2002@gmail.com>
2026-09-01 17:46:16 +0545

    reinitialize database

--- AirbnbFinal/init/index.js
+++ AirbnbFinal/init/index.js
@@ -1,27 +1,33 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
+const dbURL = process.env.MONGO_URL;
+
 main()
   .then(() => {
-    console.log("Database Connected Sucessfully");
+    console.log("Database Connected Successfully");
   })
   .catch((err) => {
     console.log(err);
   });
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/WanderlustFinal");
+  await mongoose.connect(dbURL);
 }
 
 const initialData = async () => {
   await Listing.deleteMany({});
   const dataWithOwner = initData.data.map((obj) => ({
     ...obj,
-    owner: "6a8023c33dbc958190ee52c2",
+    owner: "6a8023c33dbc958190ee52c2", // replace with a real user's _id from your DB
   }));
   await Listing.insertMany(dataWithOwner);
-  console.log("Data Initialized Sucessfully");
+  console.log("Data Initialized Successfully");
 };
 
 initialData();
