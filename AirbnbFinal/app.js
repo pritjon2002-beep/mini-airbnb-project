8e93bc37e1a5943c09ed5aecd14b27dfeadfe9c0
pritam-demo <pritjon2002@gmail.com>
2026-09-01 16:25:12 +0545

    cookie fix

--- AirbnbFinal/app.js
+++ AirbnbFinal/app.js
@@ -76,7 +76,6 @@ const sessionOption = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
   },
