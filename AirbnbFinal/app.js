a782b269cd5e31975dcc941ee050ef17dff1ebee
pritam-demo <pritjon2002@gmail.com>
2026-09-03 15:45:55 +0545

    trust proxy

--- AirbnbFinal/app.js
+++ AirbnbFinal/app.js
@@ -7,6 +7,7 @@ if (process.env.NODE_ENV != "production") {
 
 const express = require("express");
 const app = express();
+app.set("trust proxy", 1);
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
